04c7e267988cd89587218b9c260dd098176cd19b
vantercarlos <vantercarlos.caetano@cs.inf.br>
2022-10-20 21:51:59 -0400

    Trabalhando com endpoints

--- API/Program.cs
+++ API/Program.cs
@@ -2,5 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/", () => new {Name = "Vantercarlos", Age = 41});
+app.MapGet("/AddHeader", (HttpResponse response) => {
+    response.Headers.Add("Teste", "Vantercarlos");
+    return new {Name = "Vantercarlos", Age = 41};
+    });
 
 app.Run();
+
