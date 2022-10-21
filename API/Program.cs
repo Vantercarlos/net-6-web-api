7bbf7658b59eb22da4d411f6ff908de9bc89d10f
vantercarlos <vantercarlos.caetano@cs.inf.br>
2022-10-20 22:06:28 -0400

    Passando parâmetro pelo body

--- API/Program.cs
+++ API/Program.cs
@@ -6,7 +6,16 @@ app.MapPost("/", () => new {Name = "Vantercarlos", Age = 41});
 app.MapGet("/AddHeader", (HttpResponse response) => {
     response.Headers.Add("Teste", "Vantercarlos");
     return new {Name = "Vantercarlos", Age = 41};
-    });
+});
+
+app.MapPost("/saveProduct", (Product product) => {
+    return product.Code + " - " + product.Name;
+});
 
 app.Run();
+
+public class Product {
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
 
