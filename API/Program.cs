4e4613ff44ec1d65817d51718501902d2e770264
vantercarlos <vantercarlos.caetano@cs.inf.br>
2022-10-22 23:39:51 -0400

    Criando repositório de dados

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -27,6 +29,21 @@ app.MapGet("/getProductByHeader", (HttpRequest request) => {
 });
 
 app.Run();
+
+public static class ProductRepository {
+    public static List<Product> Products {get; set;}
+
+    public static void Add(Product product) {
+        if(Products == null)
+            Products = new List<Product>();
+
+        Products.Add(product);
+    }
+
+    public static Product GetBy(string code) {
+        return Products.First(p => p.Code == code);
+    }
+}
 
 public class Product {
     public string Code { get; set; }
